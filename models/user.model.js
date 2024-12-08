a7d1678d39e1251f7a74395f758fee9557b6e393
Nico <38060004+nicokinazoko@users.noreply.github.com>
2024-12-08 18:28:18 +0700

    add timestamp for user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,30 +1,35 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    first_name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    hashed_password: {
+      type: String,
+      required: true,
+    },
+    salt: {
+      type: String,
+      required: true,
+    },
   },
-  first_name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  last_name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  hashed_password: {
-    type: String,
-    required: true,
-  },
-  salt: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const userModel = mongoose.model('users', userSchema);
 
