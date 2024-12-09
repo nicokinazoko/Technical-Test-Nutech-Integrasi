40c7b8c79084b55efb50a857de05ac422f98fb73
Nico <38060004+nicokinazoko@users.noreply.github.com>
2024-12-09 11:59:59 +0700

    create model transaction history

---
+++ models/transaction_history.model.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+const transactionHistorySchema = mongoose.Schema(
+  {
+    invoice_number: {
+      default: String,
+      required: true,
+    },
+    service_id: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'services',
+    },
+    transaction_type: {
+      type: String,
+      enum: ['PAYMENT', 'TOPUP'],
+      default: 'PAYMENT',
+    },
+    total_amount: {
+      type: Number,
+      default: 0,
+    },
+    user_id: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'users',
+    },
+    status: {
+      type: String,
+      enum: ['active', 'deleted'],
+      default: 'active',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const transactionHistoryModel = mongoose.model(
+  'transaction_histories',
+  transactionHistorySchema
+);
+
+export default transactionHistoryModel;
