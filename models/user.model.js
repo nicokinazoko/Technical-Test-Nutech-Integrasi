c71564047ddb37cb5baccd0b394ce0035fa02de8
Nico <38060004+nicokinazoko@users.noreply.github.com>
2024-12-09 10:39:18 +0700

    add field profile image in user model

--- models/user.model.js
+++ models/user.model.js
@@ -25,6 +25,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    profile_image: {
+      type: String,
+      default: '',
+    },
   },
   {
     timestamps: true,
