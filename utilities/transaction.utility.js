32e0c860bbe239cdf2924ea6790c13d07edbe8d6
Nico <38060004+nicokinazoko@users.noreply.github.com>
2024-12-09 19:49:31 +0700

    create functionaliity to generate invoice number

---
+++ utilities/transaction.utility.js
@@ -0,0 +1,27 @@
+import transactionHistoryModel from '../models/transaction_history.model.js';
+import TransactionHistoryModel from '../models/transaction_history.model.js';
+import moment from 'moment';
+
+async function GenerateInvoiceNumber() {
+  const todayDate = moment.utc().format('DDMMYYYY');
+  const findLatestTransactionHistory =
+    await TransactionHistoryModel.findOne().sort({ createdAt: -1 });
+
+  let invoiceCode = `INV${todayDate}-001`;
+
+  if (
+    findLatestTransactionHistory &&
+    findLatestTransactionHistory.invoice_number?.includes(todayDate)
+  ) {
+    const latestNumberInvoice =
+      findLatestTransactionHistory.invoice_number.split('-')[1];
+    const incrementedNumber = (parseInt(latestNumberInvoice) + 1)
+      .toString()
+      .padStart(3, '0');
+    invoiceCode = `INV${todayDate}-${incrementedNumber}`;
+  }
+
+  return invoiceCode;
+}
+
+export { GenerateInvoiceNumber };
