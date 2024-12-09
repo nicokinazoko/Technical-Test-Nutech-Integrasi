2a75bbea5cb0ed07f27ea79d1c60cbc8f86994ef
Nico <38060004+nicokinazoko@users.noreply.github.com>
2024-12-09 11:07:45 +0700

    Add new field balance to store current user balance

--- models/user.model.js
+++ models/user.model.js
@@ -29,6 +29,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: '',
     },
+    balance: {
+      type: Number,
+      default: 0,
+    },
   },
   {
     timestamps: true,
