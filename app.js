5fa9fbd61734f6d8613d7651df5b89a7d594926d
Nico <38060004+nicokinazoko@users.noreply.github.com>
2024-12-10 20:18:20 +0700

    initiate cloudinary config

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cloudinary from 'cloudinary';
+
+import InformationRoutes from './routes/information.route.js';
+import MembershipRoutes from './routes/membership.route.js';
+import TransactionRoutes from './routes/transaction.route.js';
+const app = express();
+
+app.use(express.json());
+
+app.use('/', [InformationRoutes, MembershipRoutes, TransactionRoutes]);
 
 import './db/config.js';
 
@@ -7,12 +17,14 @@ dotenv.config({ path: '.env-dev' });
 
 const port = process.env.PORT || 4000;
 
-const app = express();
-
-app.use(express.json());
-
 app.get('/', (req, res) => {
   res.send('hello world');
+});
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 app.listen(port);
