59b4fe44fa141dda7861d089bc09bc1fe9cf3e04
Nico <38060004+nicokinazoko@users.noreply.github.com>
2024-12-10 22:48:55 +0700

    update function get all services to find use raw query

--- services/service.service.js
+++ services/service.service.js
@@ -1,14 +1,27 @@
-import ServiceModel from '../models/service.model.js';
+import { GenerateQueryMongoDB } from '../utilities/common.utility.js';
 
 async function GetAllServices() {
-  const services = await ServiceModel.find({ status: 'active' })
-    .select('-_id')
-    .lean();
+  // set parameter query to find
+  const parameterQuery = { status: 'active' };
+
+  // execute query
+  const services = await GenerateQueryMongoDB({
+    collection_name: 'services',
+    query: 'find',
+    parameter: parameterQuery,
+  });
 
   return {
     status: 0,
     message: 'Sukses',
-    data: services,
+    data: services.map((service) => {
+      return {
+        service_code: service?.service_code || '',
+        service_name: service?.service_name || '',
+        service_icon: service?.service_icon || '',
+        service_tariff: service?.service_tariff || 0,
+      };
+    }),
   };
 }
 
