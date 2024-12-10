eaf23b45a0b356efd2466ca152658cf694b0257c
Nico <38060004+nicokinazoko@users.noreply.github.com>
2024-12-10 11:14:17 +0700

    Update functionality register user to move validation input to membership controller

--- services/user.service.js
+++ services/user.service.js
@@ -2,65 +2,66 @@ import UserModel from '../models/user.model.js';
 import {
   CreateSalt,
   GenerateHashedPassword,
-  ValidateEmail,
-  ValidatePassword,
-  ValidateRequiredInput,
 } from '../utilities/common.utility.js';
 
-import {
-  CheckExistingUser,
-  GetOneUserBasedOnEmail,
-} from '../utilities/user.utitlity.js';
+import { GetOneUserBasedOnEmail } from '../utilities/user.utitlity.js';
 import { GetEmailFromToken } from '../utilities/login.utility.js';
 
+/**
+ * Creates a new user in the system.
+ *
+ * This function takes user details, generates a salt and hashed password,
+ * and saves the user information in the database. It ensures the email,
+ * first name, and last name are trimmed and converted to lowercase before saving.
+ * If successful, it returns a success message; otherwise, it logs and throws an error.
+ *
+ * @async
+ * @function CreateUser
+ * @param {Object} userDetails - An object containing user details.
+ * @param {string} userDetails.email - The email address of the user.
+ * @param {string} userDetails.first_name - The first name of the user.
+ * @param {string} userDetails.last_name - The last name of the user.
+ * @param {string} userDetails.password - The raw password of the user.
+ * @returns {Promise<Object>} A promise that resolves to an object with the following properties:
+ *   - `status` {number} - A status code indicating success (0 for success).
+ *   - `message` {string} - A message indicating the outcome (e.g., registration success message).
+ *   - `data` {null} - No data is returned for this function.
+ *
+ * @throws {Error} If there is an issue with the user creation process, it logs the error and throws an error with the message.
+ */
+
 async function CreateUser({ email, first_name, last_name, password }) {
-  const emailValidation = await ValidateEmail({
-    email,
-  });
+  try {
+    // generate salt for password
+    const salt = await CreateSalt();
 
-  if (emailValidation) return emailValidation;
+    // generate hashed password using salt
+    const hashedPassword = await GenerateHashedPassword(password, salt);
 
-  const checkExistingUser = await CheckExistingUser(email);
+    // define variable for create user
+    const inputDataUser = {
+      email: email ? email.trim().toLowerCase() : '',
+      first_name: first_name ? first_name.trim().toLowerCase() : '',
+      last_name: last_name ? last_name.trim().toLowerCase() : '',
+      salt,
+      hashed_password: hashedPassword,
+    };
 
-  if (checkExistingUser) return checkExistingUser;
+    // create data user
+    await UserModel.create(inputDataUser);
 
-  const firstNameValidation = await ValidateRequiredInput({
-    field_name: 'first_name',
-    value: first_name,
-  });
+    // return data user
+    return {
+      status: 0,
+      message: 'Registrasi berhasil silahkan login',
+      data: null,
+    };
+  } catch (error) {
+    // log the error
+    console.log(error);
 
-  if (firstNameValidation) return firstNameValidation;
-
-  const lastNameValidation = await ValidateRequiredInput({
-    field_name: 'last_name',
-    value: last_name,
-  });
-
-  if (lastNameValidation) return lastNameValidation;
-
-  const passwordValidation = await ValidatePassword({
-    password,
-  });
-
-  if (passwordValidation) return passwordValidation;
-
-  const salt = CreateSalt();
-  const hashedPassword = await GenerateHashedPassword(password, salt);
-  const inputDataUser = {
-    email: email ? email.trim().toLowerCase() : '',
-    first_name: first_name ? first_name.trim().toLowerCase() : '',
-    last_name: last_name ? last_name.trim().toLowerCase() : '',
-    salt,
-    hashed_password: hashedPassword,
-  };
-
-  await UserModel.create(inputDataUser);
-
-  return {
-    status: 0,
-    message: 'Registrasi berhasil silahkan login',
-    data: null,
-  };
+    throw new Error(error.message);
+  }
 }
 
 async function GetOneUserBasedOnToken({ token }) {
