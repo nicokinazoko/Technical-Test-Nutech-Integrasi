49d4415d6352d2390da91833a71c71fe12c7ebb8
Nico <38060004+nicokinazoko@users.noreply.github.com>
2024-12-10 18:27:12 +0700

    Update function create transaction to use email from middleware instead from token

--- services/transaction.service.js
+++ services/transaction.service.js
@@ -2,20 +2,19 @@ import UserModel from '../models/user.model.js';
 import TransactionHistoryModel from '../models/transaction_history.model.js';
 
 import { GetOneUserBasedOnEmail } from '../utilities/user.utitlity.js';
-import { GetEmailFromToken } from '../utilities/login.utility.js';
 import { GenerateInvoiceNumber } from '../utilities/transaction.utility.js';
 import { GetOneServiceBasedOnServiceCode } from '../utilities/service.utility.js';
 
 /**
  * Function to handle top-up balance for a user.
  *
- * This function validates the top-up amount, retrieves the user based on the provided email, 
+ * This function validates the top-up amount, retrieves the user based on the provided email,
  * updates the user's balance, generates a transaction history, and returns the updated balance.
  *
  * @async
  * @function TopUpBalanceForUser
  * @param {Object} params - Parameters required for the top-up process.
- * @param {number} params.top_up_amount - The amount to be added to the user's balance. 
+ * @param {number} params.top_up_amount - The amount to be added to the user's balance.
  *   Must be a positive number.
  * @param {string} params.email - The email address of the user whose balance will be updated.
  * @returns {Promise<Object>} A promise that resolves to an object containing:
@@ -23,7 +22,7 @@ import { GetOneServiceBasedOnServiceCode } from '../utilities/service.utility.js
  *   - `message` {string} - Success message ('Top Up Balance berhasil').
  *   - `data` {Object} - An object containing:
  *     - `balance` {number} - The updated balance of the user.
- * 
+ *
  * @throws {Error} If an error occurs during the process:
  *   - Throws a 400 error if the top-up amount is invalid or the user does not exist.
  *   - Logs the error and rethrows it for handling by higher-level functions.
@@ -94,27 +93,57 @@ async function TopUpBalanceForUser({ top_up_amount, email }) {
   }
 }
 
-async function CreateTransaction({ service_code, token }) {
-  const emailFromToken = await GetEmailFromToken({ tokenData: token });
+/**
+ * Create a transaction for a user based on a service code.
+ *
+ * This function verifies the user's existence, retrieves the requested service, checks for sufficient balance,
+ * deducts the service tariff from the user's balance, and logs the transaction in the history.
+ *
+ * @async
+ * @function CreateTransaction
+ * @param {Object} params - The parameters for the transaction.
+ * @param {string} params.service_code - The unique code for the requested service.
+ * @param {string} params.email - The email of the user initiating the transaction.
+ * @returns {Object} An object containing the transaction details and updated balance.
+ *
+ * @throws {Error} Throws an error if:
+ * - The user is not found (status 400).
+ * - The service is not found (status 400).
+ * - The user's balance is insufficient (status 400).
+ */
 
-  if (!emailFromToken) return null;
+async function CreateTransaction({ service_code, email }) {
+  // find user based on email from parameter
+  const user = await GetOneUserBasedOnEmail({ email: email });
 
-  const user = await GetOneUserBasedOnEmail({ email: emailFromToken });
+  // if user not found, then throw error
+  if (!user) {
+    const error = new Error('User not found');
+    error.status = 400;
+    throw error;
+  }
 
-  if (!user) return null;
-
+  // call function to get service based on service code
   const service = await GetOneServiceBasedOnServiceCode({ service_code });
 
-  if (!service) return null;
+  // if service not found, then throw error
+  if (!service) {
+    const error = new Error('Service atau Layanan tidak ditemukan');
+    error.status = 400;
+    throw error;
+  }
 
+  // if balance is sufficient, then throw error
   if (!user?.balance || user.balance < service.service_tariff) {
     const error = new Error('Balance tidak cukup');
     error.status = 400;
     throw error;
   }
 
+  // calculate updated balance
   const updatedBalance = (user?.balance || 0) - service.service_tariff;
 
+  // update balance in user
   await UserModel.updateOne(
     { _id: user._id },
     {
@@ -124,7 +153,10 @@ async function CreateTransaction({ service_code, token }) {
     }
   );
 
+  // generate invoice code
   const invoiceCode = await GenerateInvoiceNumber();
+
+  // define data for create history transaction
   const dataTransactionHistory = {
     invoice_number: invoiceCode,
     transaction_type: 'PAYMENT',
@@ -133,10 +165,12 @@ async function CreateTransaction({ service_code, token }) {
     service_id: service._id,
   };
 
+  // create data transaction history
   const transactionHistory = await TransactionHistoryModel.create(
     dataTransactionHistory
   );
 
+  // return transaction data
   return {
     status: 0,
     message: 'Transaksi berhasil',
