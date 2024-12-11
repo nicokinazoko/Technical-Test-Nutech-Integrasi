455f1638620823aabae285f2679186eea224937d
Nico <38060004+nicokinazoko@users.noreply.github.com>
2024-12-11 09:50:29 +0700

    update url to use mongoport from env

--- db/config.js
+++ db/config.js
@@ -7,7 +7,7 @@ dotenv.config({ path: '.env-dev' });
 const url =
   process.env.SERVER_ENV === 'railway'
     ? `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.MONGOPORT}/${process.env.DB_NAME}?authSource=admin`
-    : `mongodb://${process.env.DB_HOST}:27017/${process.env.DB_NAME}`;
+    : `mongodb://${process.env.DB_HOST}:${process.env.MONGOPORT}/${process.env.DB_NAME}`;
 
 console.log('MongoDB Connection URL:', url);
 
